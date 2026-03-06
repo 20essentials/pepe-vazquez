9e8fb572247a3eb47492ed3ddad9e1120fcc0efc
DominicDev <s0l0estudi4r@gmail.com>
2026-03-06 16:21:39 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'PEPE VÁSQUEZ';
+export let nameSongs = [
+	"JIPI JAY", /*1*/
+	"NO VALENTÍN", /*2*/
+	"RITMO DE NEGROS", /*3*/
+	"SINCERA CONFESIÓN", /*4*/
+	"PALMERO", /*5*/
+	"SE ME VAN LOS PIES", /*6*/
+	"LE DIJE A PAPÁ", /*7*/
+	"SIN UN AMOR", /*8*/
+	"LA PASIONARIA / REBECA", /*9*/
+	"EL ALACRÁN", /*10*/
+	"EL TROVADOR", /*11*/
+	"VOY CANTANDO", /*12*/
+	"RITMO NERO", /*13*/
+	"RAÍCES DEL FESTEJO", /*14*/
+	"LOS PECES EN EL RÍO", /*15*/
+	"LOS ÑORBOS", /*16*/
+	"DONDE TU VAYAS", /*17*/
+	"LA MORENA TRINIDAD", /*18*/
+	"BENDICIÓN PATERNAL", /*19*/
+	"MUEVE TU CU CÚ", /*20*/
+]
